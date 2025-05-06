38e2c50bb701976bb95deede70886adce31057a2
Mate18atorrante <luk.18.cardozo@gmail.com>
2025-05-06 17:40:31 -0300

    1.2.3 en python terminado

--- Subacciones/funciones y procedimientos/1.2.3.py
+++ Subacciones/funciones y procedimientos/1.2.3.py
@@ -1,8 +1,10 @@
+#Diseñe una función que permita ingresar 3 números y devuelva el mínimo valor. El programa principal debe permitir
+#que este proceso se repita la cantidad de veces que el usuario desee.
 
-def MinimoValor(num1, num2, num3):
-    if num1 <= num2 and (num2 <= num3):
+def minimo_valor(num1, num2, num3):
+    if (num1 <= num2) and (num1 <= num3):
         return num1
-    elif (num2 <= num1) and (num2 <= num3):
+    elif num2 <= num3:
         return num2
     else:
         return num3
@@ -11,13 +13,26 @@ def MinimoValor(num1, num2, num3):
 print("Este algoritmo pide tres números distintos y devuelve el número menor")
 
 while True:
+    numero1 = numero2 = numero3 = 0.0
     while True:
-        entrada = input("Ingrese los tres números (separados por coma ',': ").split(",")
-        partes = entrada.split("/")
-    try:
-        numero1, numero2, numero3 = entada.split(",")
-        numero1 = float
-        numero2 = float
-        numero3 =  float
-    print("El número menor es ", MinimoValor(numero1, numero2, numero3))
-    corte = str(input("¿Desea continuar con otros tres números? S/N: "))
+        entrada = input("\nIngrese los tres números (separados por coma ','): ").split(",")
+
+        if len(entrada) != 3:
+            print("\nPor favor, ingrese exactamente tres números.")
+            continue
+
+        try:
+            numero1, numero2, numero3 = map(lambda x: float(x.strip()), entrada)
+            break
+        except ValueError:
+            print("\nLos valores deben de ser números, no carácteres.")
+
+    print("El número menor es ", minimo_valor(numero1, numero2, numero3))
+    while True:
+        corte = input("¿Desea continuar con otros tres números? S/N: ").strip().upper()
+        if corte in ("N", "S"):
+            break
+        else:
+            print("Por favor, ingrese solo 'S' para continuar o 'N' para terminar")
+    if corte == "N":
+        break
